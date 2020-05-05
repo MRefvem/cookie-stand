54fb0701202a7e1d58f8d290df1434383c05ac8e
MichaelRefvem <mkrefvem@gmail.com>
2020-05-05 14:29:11 -0700

    first edit to branch, bulding my constructor functions

--- js/app2.js
+++ js/app2.js
@@ -1,5 +1,86 @@
 'use strict'
 
+// Lab: Salmon Cookies Pt. 2
+
+// Instructions
+// 1. Create a new branch for today's lab. Make sure it has all of your changes from lab 06 so that you can extend the functionality. (complete)
+// 2. Replace all of your object literals for the salmon cookie stand with a single constructor function that, when called with the 'new' keyword, creates a new instance.
+// 3. Replace the lists of your data for each store and build a single table of data instead. Display each store's data in a table format. Break each column by the hour and complete each row with a "Daily Location Total".
+
+var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'total'];
+
+function Location(name, minCustomers, maxCustomers, avgCookies, customersPerHour, cookiesPerHour, dailyTotal){
+  this.name = name;
+  this.minCustomers = minCustomers;
+  this.maxCustomers = maxCustomers,
+  this.avgCookies = avgCookies;
+  this.customersPerHour = customersPerHour;
+  this.cookiesPerHour = cookiesPerHour;
+  this.dailyTotal = dailyTotal;
+}
+
+var seattle = new Location('Seattle', 23, 65, 6.3, [], [], 0);
+var tokyo = new Location('Tokyo', 3, 24, 1.2, [], [], 0);
+var dubai = new Location('Dubai', 11, 38, 3.7, [], [], 0);
+var paris = new Location('Paris', 20, 38, 2.3, [], [], 0);
+var lima = new Location('Lima', 2, 16, 4.6, [], [], 0);
+
+console.log(seattle, tokyo, dubai, paris, lima);
+
+var Seattle = {
+  name: 'Seattle',
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookies: 6.3,
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'total'],
+  customersPerHour: [],
+  cookiesPerHour: [],
+  dailyTotal: 0,
+  randomCustomersPerHour: function(){
+    for (var i = 0; i < this.hoursOfOperation.length; i++){
+      var randomCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
+      this.customersPerHour.push(randomCustomers);
+    }
+    console.log('Seattle: Customers per hour',this.customersPerHour);
+  },
+  findCookiesPerHour: function(){ 
+    for (var i = 0; i < this.hoursOfOperation.length-1; i++){
+      var perHour = Math.round(this.avgCookies * this.customersPerHour[i]);
+      this.cookiesPerHour.push(perHour);
+      this.dailyTotal = this.dailyTotal + perHour;
+    }
+    console.log('Seattle: Cookies per hour',this.cookiesPerHour);
+  },
+  sumCookiesPerDay: function(){
+    var dailyTotal = 0;
+    for (var i = 0; i < this.cookiesPerHour.length; i++) {
+      dailyTotal = this.cookiesPerHour[i] + dailyTotal;
+    }
+    console.log('Seattle: Cookies per day',this.dailyTotal);
+    this.cookiesPerHour.push(dailyTotal);
+  },
+  render: function(){
+  var parent = document.getElementById('seattle-sales-data');
+  var listItem = document.createElement('p');
+  listItem.textContent = this.name;
+  parent.appendChild(listItem);
+  },
+  renderList: function(){
+  var parent = document.getElementById('seattle-sales-data');
+  for (var i = 0; i < this.hoursOfOperation.length; i++){
+    var listItem = document.createElement('li');
+    listItem.textContent = (`${this.hoursOfOperation[i]}: ${this.cookiesPerHour[i]} cookies`);
+    parent.appendChild(listItem);
+  }
+  }
+}
+
+Seattle.randomCustomersPerHour();
+Seattle.findCookiesPerHour();
+Seattle.sumCookiesPerDay();
+Seattle.render();
+Seattle.renderList();
+
 
 // SEATTLE
 
